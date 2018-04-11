824346efc5488746178b6306d152fab4ff3ddaae
BorisAnimal <iguru98@yandex.ru>
2018-04-11 23:03:43 +0300

    Added EntitySelect MVC

---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectController.java
@@ -0,0 +1,12 @@
+package com.ba.yo.innovativepasswordmanager;
+
+/**
+ * Created by Ford on 4/11/2018.
+ */
+
+public class EntitySelectController implements EntitySelectMVC.Controller{
+    @Override
+    public void getData() {
+        //TODO
+    }
+}
---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectMVC.java
@@ -0,0 +1,27 @@
+package com.ba.yo.innovativepasswordmanager;
+
+/**
+ * Created by Ford on 4/11/2018.
+ */
+
+public interface EntitySelectMVC {
+    interface View extends Notifiable {
+        /**
+         * @param description - description of account that user defined
+         * @param id          - hide it in element (such in 'tag' field). Use it in intent when item selected
+         */
+        void addEntity(String description, String id);
+
+        /**
+         * Remove all items from list
+         */
+        void clearList();
+    }
+
+    interface Controller {
+        /**
+         * Asks server for current list of accounts
+         */
+        void getData();
+    }
+}

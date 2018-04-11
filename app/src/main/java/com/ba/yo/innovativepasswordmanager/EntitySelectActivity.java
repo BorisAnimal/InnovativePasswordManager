bd594943040219d8f00e8f526244feaba956889a
Maxim Surkov <modbrin@gmail.com>
2018-04-12 02:22:26 +0300

    Added Couple of Activities
    +Entry Activity xml
    +Chenge Master Password Activity xml
    +Starter Layout for Entity Select Activity
    +Transitions between existing ectivities (default animation yet)
    -All old xml's

--- app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectActivity.java
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectActivity.java
@@ -1,5 +1,6 @@
 package com.ba.yo.innovativepasswordmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
@@ -14,11 +15,6 @@ public class EntitySelectActivity extends AppCompatActivity {
 
     //TODO: implement EntitySelectMVC.View interface
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +22,14 @@ public class EntitySelectActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addEntry = (FloatingActionButton) findViewById(R.id.addEntry);
+        addEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(EntitySelectActivity.this, EditEntryActivity.class));
             }
         });
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     @Override
@@ -55,16 +47,11 @@ public class EntitySelectActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.edit_master_password) {
+            startActivity(new Intent(EntitySelectActivity.this, EditMasterPasswordActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }

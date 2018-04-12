8c6763ec102bb3b50fd1f51706675c06cf23f8c5
BorisAnimal <iguru98@yandex.ru>
2018-04-12 11:58:55 +0300

    log updated

--- app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectController.java
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/EntitySelectController.java
@@ -15,6 +15,7 @@ import retrofit2.Response;
 public class EntitySelectController implements EntitySelectMVC.Controller {
     private EntitySelectMVC.View view;
     private ApiClient api;
+    private final String TAG = "SELECT_CONTROLLER";
 
     public EntitySelectController(EntitySelectMVC.View view) {
         this.view = view;
@@ -27,8 +28,8 @@ public class EntitySelectController implements EntitySelectMVC.Controller {
         call.enqueue(new Callback<List<EntityAccountModel>>() {
             @Override
             public void onResponse(Call<List<EntityAccountModel>> call, Response<List<EntityAccountModel>> response) {
-                Log.d("HELLO!", response.toString());
-                Log.d("HELLO!", response.body().size() + "");
+                Log.d(TAG, response.toString());
+                Log.d(TAG, response.body().size() + "");
                 if (response.body() != null) {
                     List<EntityAccountModel> entities = response.body();
                     for (EntityAccountModel ent : entities) {
@@ -41,7 +42,7 @@ public class EntitySelectController implements EntitySelectMVC.Controller {
 
             @Override
             public void onFailure(Call<List<EntityAccountModel>> call, Throwable t) {
-                Log.d("HELLO!", t.getMessage());
+                Log.d(TAG, t.getMessage());
                 view.showNotification(t.getMessage());
                 call.cancel();
             }

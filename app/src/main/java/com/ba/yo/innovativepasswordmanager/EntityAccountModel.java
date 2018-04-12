067c9e2d0075b1bd7f8f63b50dd3916352f5df6b
BorisAnimal <iguru98@yandex.ru>
2018-04-12 11:26:25 +0300

    Select controller

---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/EntityAccountModel.java
@@ -0,0 +1,36 @@
+package com.ba.yo.innovativepasswordmanager;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Java-Ai-BOT on 4/11/2018.
+ */
+
+public class EntityAccountModel {
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("id")
+    private String id;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public EntityAccountModel(String description, String id) {
+        this.description = description;
+        this.id = id;
+    }
+}

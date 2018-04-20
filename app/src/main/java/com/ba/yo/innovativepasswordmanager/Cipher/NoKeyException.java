d8ad176a12d80410ee83b129b2467151e6495975
BorisAnimal <iguru98@yandex.ru>
2018-04-20 21:22:17 +0300

    fun with tests

---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/Cipher/NoKeyException.java
@@ -0,0 +1,4 @@
+package com.ba.yo.innovativepasswordmanager.Cipher;
+
+public class NoKeyException extends Exception {
+}
---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/controllers/AppletSelectController.java
@@ -0,0 +1,19 @@
+package com.ba.yo.innovativepasswordmanager.controllers;
+
+import com.ba.yo.innovativepasswordmanager.AppletSelectMVC;
+
+/**
+ * Created by Java-Ai-BOT on 4/20/2018.
+ */
+
+public class AppletSelectController implements AppletSelectMVC.Controller {
+    @Override
+    public void getData() {
+        
+    }
+
+    @Override
+    public void sendDataToApplet(String appletId, String accountId) {
+
+    }
+}

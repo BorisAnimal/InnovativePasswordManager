3d99efcb0ae451a81c9e4137eb9cbbe75b068c03
BorisAnimal <iguru98@yandex.ru>
2018-04-20 20:01:37 +0300

    mvc added

---
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/DumpDataMVC.java
@@ -0,0 +1,15 @@
+package com.ba.yo.innovativepasswordmanager;
+
+/**
+ * Created by Java-Ai-BOT on 4/19/2018.
+ */
+
+public interface DumpDataMVC {
+    interface View {
+
+    }
+
+    interface Controller {
+
+    }
+}

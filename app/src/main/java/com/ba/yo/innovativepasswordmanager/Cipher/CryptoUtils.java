6e56f6df93b6bedf121d01408b8e663e917a2dac
BorisAnimal <iguru98@yandex.ru>
2018-04-26 06:51:30 +0300

    Revert "before NDK rejection"
    This reverts commit d4212d1ee1c57cdc9575b4c541fae41832d79163.

--- app/src/main/java/com/ba/yo/innovativepasswordmanager/Cipher/CryptoUtils.java
+++ app/src/main/java/com/ba/yo/innovativepasswordmanager/Cipher/CryptoUtils.java
@@ -1,28 +1,19 @@
 package com.ba.yo.innovativepasswordmanager.Cipher;
 
 import javax.crypto.*;
-import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 
 public class CryptoUtils {
-    private static final String HASH_ALGO = "SHA-256";
-    private static final String CYPHER_ALGO = "AES/GCM/NoPadding";
-    private static final String CYPHER_SEC_KEY_ALGO = "AES";
-    private static final String PASS_HASH_ALGO = "PBKDF2WithHmacSHA1";
-    private static final byte[] SALT = "leaderSucciLeadsUsToVictory".getBytes();
-    private static final int ITERATIONS = 100_000;
-    private static final int KEY_LENGTH = 32;
-
-    private static String lastKey = null;
-    private static SecretKeySpec keySpec = null;
+    private static final String CYPHER_ALGO = "AES";
+    private static final String RANDOM_ALGO = "PBKDF2WithHmacSHA1";
+    private static final byte[] SALT = {-74, -57, 117, 106, -69, 125, 88, 62, 56, -11, -110, 21,
+            -51, -117, -71, 3, -90, -75, -19, 38, -34, 93, 40, -21};
+    private static final int ITERATIONS = 1000;
+    private static final int KEY_LENGTH = 24;
 
     /**
      * Example of CryptoUtils class usage.
@@ -33,7 +24,7 @@ public class CryptoUtils {
     public static void main(String[] args) throws DecryptionException {
         System.out.println("========== CryptoUtils example of usage ==========");
         String MESSAGE = "Hello, world!";
-        String PASSWORD = "very_complicated_password";
+        String PASSWORD = "complicated_password";
         System.out.println("Message: \"" + MESSAGE + "\"");
         System.out.println("Password: \"" + PASSWORD + "\"");
 
@@ -48,26 +39,9 @@ public class CryptoUtils {
     }
 
     /**
-     * Get hash representation of a string (default SHA-256).
-     *
-     * @param input String to hash
-     * @return Hashed string in form of bytes
-     */
-    public static byte[] getHash(String input) {
-        try {
-            return MessageDigest.getInstance(HASH_ALGO).digest(input.getBytes(StandardCharsets.UTF_8));
-        } catch (NoSuchAlgorithmException e) {
-            // only if algorithm name will be changed in a wrong way
-            e.printStackTrace();
-            System.exit(-1);
-            return null;  // unreachable
-        }
-    }
-
-    /**
      * Get closely undecipherable representation of the string according to the key.
      *
-     * @param key     Key for the encryption
+     * @param key     Key for cyphering the message (important to store somewhere or memorize)
      * @param message Message to encrypt
      * @return Encrypted version of message (maybe decrypted using 'decrypt' method)
      */
@@ -75,16 +49,13 @@ public class CryptoUtils {
         if (key == null || message == null) {
             throw new NullPointerException();
         }
-        if (lastKey == null || !lastKey.equals(key)) {
-            setKey(key);
-        }
-        return toHex(encrypt(message.getBytes()));
+        return toHex(encrypt(getRawKey(key.toCharArray()), message.getBytes()));
     }
 
     /**
      * Get the original representation of the string according to the key.
      *
-     * @param key       Key for the decryption
+     * @param key       Key for cyphering the message (important to store somewhere or memorize)
      * @param encrypted Encrypted version of the original message
      * @return Original message as it was before encryption using method 'encrypt'
      * @throws DecryptionException Error during decryption
@@ -93,38 +64,29 @@ public class CryptoUtils {
         if (key == null || encrypted == null) {
             throw new NullPointerException();
         }
-        if (lastKey == null || !lastKey.equals(key)) {
-            setKey(key);
-        }
-        return new String(decrypt(toByte(encrypted)));
+        return new String(decrypt(getRawKey(key.toCharArray()), toByte(encrypted)));
     }
 
-    private static void setKey(String key) {
+    private static byte[] getRawKey(char[] seed) {
         try {
 
-            SecretKeyFactory f = SecretKeyFactory.getInstance(PASS_HASH_ALGO);
-            KeySpec spec = new PBEKeySpec(key.toCharArray(), SALT, ITERATIONS, KEY_LENGTH * 8);
-            SecretKey secretKey = f.generateSecret(spec);
-            keySpec = new SecretKeySpec(secretKey.getEncoded(), CYPHER_SEC_KEY_ALGO);
-            lastKey = key;
+            PBEKeySpec spec = new PBEKeySpec(seed, SALT, ITERATIONS, KEY_LENGTH * 8);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(RANDOM_ALGO);
+            return skf.generateSecret(spec).getEncoded();
 
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             e.printStackTrace();
             System.exit(-1);
+            return new byte[]{};  // unreachable
         }
     }
 
-    private static byte[] encrypt(byte[] message) {
+    private static byte[] encrypt(byte[] rawKey, byte[] message) {
         try {
 
             Cipher cipher = Cipher.getInstance(CYPHER_ALGO);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            byte[] iv = cipher.getIV();
-            byte[] cipherText = cipher.doFinal(message);
-            byte[] res = new byte[12 + message.length + 16];
-            System.arraycopy(iv, 0, res, 0, 12);
-            System.arraycopy(cipherText, 0, res, 12, cipherText.length);
-            return res;
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(rawKey, CYPHER_ALGO));
+            return cipher.doFinal(message);
 
         } catch (InvalidKeyException
                 | IllegalBlockSizeException
@@ -137,17 +99,17 @@ public class CryptoUtils {
         }
     }
 
-    private static byte[] decrypt(byte[] encrypted) throws DecryptionException {
+    private static byte[] decrypt(byte[] rawKey, byte[] encrypted) throws DecryptionException {
         try {
 
             Cipher cipher = Cipher.getInstance(CYPHER_ALGO);
-            GCMParameterSpec params = new GCMParameterSpec(128, encrypted, 0, 12);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, params);
-            return cipher.doFinal(encrypted, 12, encrypted.length - 12);
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(rawKey, CYPHER_ALGO));
+            return cipher.doFinal(encrypted);
 
         } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            System.out.println(e.getLocalizedMessage());
             throw new DecryptionException();
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
             // only if algorithm name will be changed in a wrong way
             e.printStackTrace();
             System.exit(-1);
@@ -155,29 +117,7 @@ public class CryptoUtils {
         }
     }
 
-    /**
-     * Get hex representation of a bytes array.
-     *
-     * @param bytes Bytes array to represent as a string
-     * @return String representation of bytes array
-     */
-    protected static String toHex(byte[] bytes) {
-        if (bytes == null)
-            return "";
-        StringBuffer res = new StringBuffer(2 * bytes.length);
-        for (byte aBuf : bytes) {
-            appendHex(res, aBuf);
-        }
-        return res.toString();
-    }
-
-    /**
-     * Get byte representation of a hex string.
-     *
-     * @param hexString Bytes array to represent as a string
-     * @return String representation of bytes array
-     */
-    protected static byte[] toByte(String hexString) {
+    private static byte[] toByte(String hexString) {
         int len = hexString.length() / 2;
         byte[] res = new byte[len];
         for (int i = 0; i < len; i++)
@@ -185,7 +125,17 @@ public class CryptoUtils {
         return res;
     }
 
-    private final static String HEX = "0123456789abcdef";
+    protected static String toHex(byte[] buf) {
+        if (buf == null)
+            return "";
+        StringBuffer res = new StringBuffer(2 * buf.length);
+        for (byte aBuf : buf) {
+            appendHex(res, aBuf);
+        }
+        return res.toString();
+    }
+
+    private final static String HEX = "0123456789ABCDEF";
 
     private static void appendHex(StringBuffer sb, byte b) {
         sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
@@ -193,7 +143,6 @@ public class CryptoUtils {
 
     /**
      * Error of CryptoUtils.decrypt method
-     * 88995cf00ac5e130b3d8f50d68f166ff7564814db8a958fa0e3dc5b5ff875b25c1ab307213c722f873524f6f7746376fe8d675235b613ecde682fd06b915646f2061f832f82c
      */
     public static class DecryptionException extends Exception {
     }
